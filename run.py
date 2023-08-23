91a9e826cfb39346f7e0afb1a83bd16bc4f013af
Fabricio Puente M <fpuentem@visiontechconsulting.ca>
2023-08-23 17:06:19 -0300

    Add comments a videos

--- run.py
+++ run.py
@@ -6,3 +6,12 @@ mpost = ctypes.cdll.LoadLibrary('./Debug/libMPOST_Linux.so')
 
 mpost.CreateAcceptor()
 mpost.RunLoop()
+
+# Other documents an links
+## Python code
+# https://github.com/PyramidTechnologies/Python-RS-232/blob/master/host.py#L185
+# https://stackoverflow.com/questions/52403928/mei-cash-validator-programming-with-python
+
+## Video links
+# https://www.youtube.com/watch?v=l4IrCwFUXfc
+# https://www.youtube.com/watch?v=AZYfosDKm2o
